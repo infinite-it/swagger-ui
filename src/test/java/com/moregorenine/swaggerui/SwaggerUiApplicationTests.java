8c3483e477cda69dae5062f8c0bb3176d5c37909
moregorenine <moregorenine@gmail.com>
2022-01-22 01:04:54 +0900

    init project

---
+++ src/test/java/com/moregorenine/swaggerui/SwaggerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moregorenine.swaggerui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
